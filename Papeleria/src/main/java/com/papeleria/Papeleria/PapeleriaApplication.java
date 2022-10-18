3d94e76ae97bfc94138b9e8031806f4411096dee
Danilo0528 <80805423+Danilo0528@users.noreply.github.com>
2022-10-17 19:56:29 -0500

    Creacion del proyecto base

---
+++ Papeleria/src/main/java/com/papeleria/Papeleria/PapeleriaApplication.java
@@ -0,0 +1,13 @@
+package com.papeleria.Papeleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapeleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapeleriaApplication.class, args);
+	}
+
+}
