709c73739caeb202b654defdc5acdd6d650b26e7
Danilo0528 <80805423+Danilo0528@users.noreply.github.com>
2022-10-30 02:36:35 -0500

    corregir el fecth

--- Papeleria/src/main/resources/static/Producto.js
+++ Papeleria/src/main/resources/static/Producto.js
@@ -1,20 +1,52 @@
 document.addEventListener("DOMContentLoaded",() =>{ //apenas cargue el html cargue esto
 //console.log("Comenzar a codificar"); 
-const Producto = document.querySelector("#producto"); 
-console.log(Producto);
+const Producto = document.querySelector("#producto");
+const ProductoForm = document.querySelector("#create-producto") //creamos una constante y una url segundaria
+ProductoForm.addEventListener("submit",createProducto); //toma los datos al presionar submit ejecuta el create proveedor
+//console.log(ProductoForm); los console son pra mostrar por consola si esta funcionando 
+//console.log(Producto);
 fetch("http://localhost:8080/producto/all") 
     .then(response => response.json()) 
     //.then(response => console.log(response)) 
     .then(Producto => Producto.forEach(slapItOnTheDOM)) 
 function slapItOnTheDOM(producto){ 
     //console.log("insertando el listado de proveedores"); 
-    const productoLi = document.createElement("li"); 
+    const productoLi = document.createElement("li");  //crea una lista
     productoLi.dataset.idProducto = producto.idProducto;  
-    productoLi.innerHTML = '<span>'+ producto.nombreProducto+' - '+producto.detalleProducto+'</span>'
+    productoLi.innerHTML = '<span>'+ producto.idproducto + " - " + producto.nombreProducto +' - '+producto.detalleProducto+'</span>'
     Producto.appendChild(productoLi); 
 //console.log("Identificador del proveedor",producto.idProducto);
 
-} })
+}
+function createProducto (event){  //ver linea 4
+    console.log("vamos a crear un producto")
+    event.preventDefault(); //espera hasta que termine la funcion
+    let nuevoProducto = reunirFormData();//reune los datos del formulario
+    return fetch ("http://localhost:8080/producto.html" ,{ //Creamos otra lista
+    method:"POST" ,
+    headers: {
+        'Content_Type': 'aplication/json',
+        'Acept': 'Aplication/Json'
+    },
+    body: JSON.stringify(nuevoProducto) //mostramos los datos de la lista
+
+    } )
+     .then(res => res.json()) //la promesa ejecuta todo la lista
+     .then(producto => (slapItOnTheDOM(producto))) //llamamos a la funcion slapitonthedom para que me muestre mi producto
+    
+    }
+    function reunirFormData (){ 
+        return {  //lo adicionamos por un error 
+        idProducto: event.target.idProducto.value,  //index html genera el evento para tomar los datos ingresados
+        nombreProducto: event.target.nombreProducto.value,
+        detalleProducto: event.target.detalleProducto.value
+    }
+    }
+})
+
+
+
+
 
 /*
 //document.addEventListener('DOMContentLoaded', (event) => {
